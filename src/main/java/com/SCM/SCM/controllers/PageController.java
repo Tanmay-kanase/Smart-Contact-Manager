177ef4ed1ab8f8849ff7651bf5504f0c626cdb21
Tanmay Kanase <tanmaykanase06@gmial.com>
2024-11-18 10:34:35 +0530

    Thymleaf Fragment learned

--- src/main/java/com/SCM/SCM/controllers/PageController.java
+++ src/main/java/com/SCM/SCM/controllers/PageController.java
@@ -1,15 +1,35 @@
 package com.SCM.SCM.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 @Controller
 public class PageController {
         
     @RequestMapping("/home")
-    public String home(){
+    public String home(Model model){
         System.out.println("Home page handler");
+        model.addAttribute("name","Tanmay Kanase");
+        model.addAttribute("Profesion", "Software Engineer");
      return "home";
+    }
+
+    // aboute route
+    @RequestMapping("/about")
+    public String aboutPage() {
+        System.out.println("About page");
+        return "about";
+    }
+
+    //service
+    @RequestMapping("/services")
+    public String servicesPage() {
+        System.out.println("Service page");
+        return "services";
     }
 }
