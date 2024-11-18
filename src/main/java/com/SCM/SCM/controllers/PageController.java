6b50b5765606e15d0318ac044712c44d1d3566a4
Tanmay Kanase <tanmaykanase06@gmial.com>
2024-11-18 08:05:27 +0530

    Initial Commit

---
+++ src/main/java/com/SCM/SCM/controllers/PageController.java
@@ -0,0 +1,15 @@
+package com.SCM.SCM.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class PageController {
+        
+    @RequestMapping("/home")
+    public String home(){
+        System.out.println("Home page handler");
+     return "home";
+    }
+}
